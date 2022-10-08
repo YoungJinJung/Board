547e9909e946905f6292e988e4fd7ff75a7a394a
YoungJinJung <yj.jung5078@gmail.com>
2022-10-08 22:25:02 +0900

    modify code

--- script/startup.sh
+++ script/startup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ec2-user/build/*.jar)
-JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
+BUILD_WAR=$(ls /home/ec2-user/build/*.war)
+WAR_NAME=$(basename $BUILD_WAR)
+echo "> build 파일명: WAR_NAME" >> /home/ec2-user/deploy.log
 
 echo "> build 파일 복사" >> /home/ec2-user/deploy.log
 DEPLOY_PATH=/home/ec2-user/
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+CURRENT_PID=$(pgrep -f WAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
@@ -19,8 +19,8 @@ else
   sleep 5
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
+BUILD_WAR=$DEPLOY_PATHWAR_NAME
+echo "> BUILD_WAR 배포"    >> /home/ec2-user/deploy.log
+nohup java -jar BUILD_WAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
 
 
--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -2,13 +2,18 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-public class BoardApplication {
+public class BoardApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
+	}
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
 	}
 
 }
